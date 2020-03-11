26cdea2c8b98dc39eeeed751176ae44f80a2f632
Ben Olsen <olsenben1992@gmail.com>
2020-03-10 18:21:15 -0700

    added schema

--- db/schema.sql
+++ db/schema.sql
@@ -2,3 +2,8 @@ DROP DATABASE IF EXISTS highscore_db;
 
 CREATE DATABASE highscore_db;
 
+CREATE TABLE highscores (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    highscore_name VARCHAR(30) NOT NULL,
+    devoured BOOLEAN DEFAULT false,
+);
